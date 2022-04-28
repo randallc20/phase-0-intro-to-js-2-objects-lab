b4029603c46cfefeb29720885f40f1be4bc9dca5
chrisrandall <chris.f.randall@gmail.com>
2022-04-27 19:26:46 -0500

    commit tree 03bbd28ee49ee7b96378e348083978c517cf044d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Christopher Randall",
+    streetAddress: "2344 Timber Lane",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    //create clone of obj
+    const updatedEmployee = {...employee};
+    //update key and value
+    updatedEmployee[key] = value;
+    //return
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
